56ffdca24b66902b4ad8aeb98f916c45f233abaa
shredder <atbinder42@gmail.com>
2023-01-05 19:25:47 -0600

    continued set up

--- route_cipher_encrypt.py
+++ route_cipher_encrypt.py
@@ -1,21 +1,32 @@
 '''
-get dummy words
-get key
-convert plaintext to all caps
-replace codewords
+validate and convert plaintext to all caps
+validate key and turn it to integers
+replace codewords and convert to all caps
+validate dummy words and convert to all caps
+build matrix
+encrypt and return encrypted message
 '''
 
 
 #--------------------------------------------------
+# USER INPUT
 
 # the sting to be encrypted (paste between quotes):
 plaintext = '''We will run the batteries at Vicksburg the night of April 16
 and proceed to Grand Gulf where we will reduce the forts. Be prepared to cross
 the river on April 25 or 29. Admiral Porter'''
 
-# matrix is 6 rows x 7 columns
-# make up own dummy words
-# key is [-1, 3, -2, 6, 5, -4]
+# or rows in the transposition matrix
+ROWS = 6
+
+# of columns in the transposition matrix
+COLS = 7
+
+# include dummy words separated by spaces equal to the number of columns (paste between quotes):
+dummy_words = 'angels crocodile cabin fire university spain'
+
+# key with spaces between numbers; negative to read UP column (ex = -1 2 -3 4):
+key = '-1 3 -2 6 5 -4'
 
 #--------------------------------------------------
 
